0deb2db46de092876020bd52809aff9b3993f711
Danimanza2005 <danielgarciamanza@gmail.com>
2025-06-05 15:01:52 +0200

    Modificacion react

--- FRONTEND/tfg_react/src/App.jsx
+++ FRONTEND/tfg_react/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Registro from "./componentes/Registro";
-import Login from "./componentes/Login";
-import Dashboard from "./componentes/Dashboard";
+import Registro from "./componentesSesion/Registro";
+import Login from "./componentesSesion/Login";
+import Dashboard from "./componentesSesion/Dashboard";
 import ProtectedRoute from "./protegida/ProtectedRoute";
 import Navbar from "./protegida/Navbar";
+import CrearPartido from "./componentes/CrearPartido";
 
 function App() {
   return (
@@ -12,14 +13,22 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registro />} />
-        <Route
-          path="/dashboard"
+        <Route path="/dashboard"
           element={
             <ProtectedRoute>
               <Dashboard />
             </ProtectedRoute>
           }
         />
+        <Route path="/crear-partido"
+          element={
+            <ProtectedRoute>
+              <CrearPartido />
+            </ProtectedRoute>
+          }
+        />
+
+        {/*Para que si no existe la ruta te redirija al login*/}
         <Route path="*" element={<Login />} />
       </Routes>
     </Router>
